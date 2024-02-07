d1fb651aa1f673559ded6e3de9da82ffd713caeb
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2024-02-06 16:24:48 -0800

    Refactored project and added task title input field

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -2,6 +2,7 @@ package edu.ucsd.cse110.successorator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +10,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import edu.ucsd.cse110.successorator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding view;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
 
-        var view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
-        view.placeholderText.setText(R.string.hello_world);
-
+        this.view = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(view.getRoot());
     }
 }
