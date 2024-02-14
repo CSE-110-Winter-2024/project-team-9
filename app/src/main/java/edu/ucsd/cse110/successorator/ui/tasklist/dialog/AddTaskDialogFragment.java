f03d53f8f795ce8276add69b9162bcbb5c3f8d33
Anchit Kumar <ank016@ucsd.edu>
2024-02-13 22:50:50 -0800

    make taskText non null

--- app/src/main/java/edu/ucsd/cse110/successorator/ui/tasklist/dialog/AddTaskDialogFragment.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/ui/tasklist/dialog/AddTaskDialogFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -67,11 +68,13 @@ public class AddTaskDialogFragment extends DialogFragment {
 
     private void onPositiveButtonClick() {
         // variable that stores the text inputted when check icon is pressed
-        String taskText = editTextTask.getText().toString();
+        @NonNull String taskText = editTextTask.getText().toString();
+        Log.d("onPositiveButtonClick", taskText);
         dismiss();
     }
 
     private void onNegativeButtonClick(DialogInterface dialog, int which) {
+        Log.d("onNegativeButtonClick", "cancel");
         dialog.cancel();
     }
 }
