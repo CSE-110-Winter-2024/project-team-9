5001e760f7cd5ba35341d0de9c986614a89f31ad
Murou Wang <muwang@ucsd.edu>
2024-02-14 15:43:05 -0800

    Converter for LocalDate
    Build failed b/c Room doesn't recognize LocalDate type, made converter to a string.

--- app/src/main/java/edu/ucsd/cse110/successorator/data/db/SuccessoratorDatabase.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/data/db/SuccessoratorDatabase.java
@@ -2,7 +2,9 @@ package edu.ucsd.cse110.successorator.data.db;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+@TypeConverters({LocalDateConverter.class})
 @Database(entities = {TaskEntity.class}, version = 1)
 public abstract class SuccessoratorDatabase extends RoomDatabase{
     public abstract TaskDao taskDao();
