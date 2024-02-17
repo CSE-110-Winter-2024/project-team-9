b7ccbb88e55b31478e63b6b80e8da1ac46419fc9
Anchit Kumar <ank016@ucsd.edu>
2024-02-16 19:45:44 -0800

    tests for task list and adding new task

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,13 @@ android {
 dependencies {
     implementation(project(":lib"))
     implementation(project(":lib"))
+    implementation(project(":lib"))
     project(":lib")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:2.25.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
