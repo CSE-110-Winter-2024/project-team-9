c317772cd78083a77a6f8b7aac34b48906457ce5
andrewzpu <66804382+andrewzpu@users.noreply.github.com>
2024-02-28 01:12:37 -0800

    Added testing for US7

---
+++ app/src/androidTest/java/edu/ucsd/cse110/successorator/SwitchViewTest.java
@@ -0,0 +1,120 @@
+package edu.ucsd.cse110.successorator;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SwitchViewTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void switchViewTest() {
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.header_bar_dropdown), withContentDescription("Dropdown"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(com.google.android.material.R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction viewGroup = onView(
+                allOf(withParent(allOf(withId(android.R.id.custom),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        isDisplayed()));
+        viewGroup.check(matches(isDisplayed()));
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.pending_button), withText("Pending List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.custom),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withText("Pending"),
+                        withParent(allOf(withId(com.google.android.material.R.id.action_bar),
+                                withParent(withId(com.google.android.material.R.id.action_bar_container)))),
+                        isDisplayed()));
+        textView.check(matches(withText("Pending")));
+
+        ViewInteraction actionMenuItemView2 = onView(
+                allOf(withId(R.id.header_bar_dropdown), withContentDescription("Dropdown"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(com.google.android.material.R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.recurring_button), withText("Recurring List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.custom),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withText("Recurring"),
+                        withParent(allOf(withId(com.google.android.material.R.id.action_bar),
+                                withParent(withId(com.google.android.material.R.id.action_bar_container)))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Recurring")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
